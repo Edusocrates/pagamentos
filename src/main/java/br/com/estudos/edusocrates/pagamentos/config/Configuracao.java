bccc5aa05c0cf2216d1e01e6af2aa057412914f9
edusocrates <edu5390@gmail.com>
2023-08-13 18:12:18 -0300

    inserindo configuracoes do modelMapper como um Bean

---
+++ src/main/java/br/com/estudos/edusocrates/pagamentos/config/Configuracao.java
@@ -0,0 +1,18 @@
+package br.com.estudos.edusocrates.pagamentos.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Configuracao {
+
+
+
+    @Bean
+    public ModelMapper obterModelMapper() {
+        return new ModelMapper();
+    }
+
+
+}
